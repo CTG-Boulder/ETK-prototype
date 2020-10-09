67653b613489794ffc28db808017b8bac16eb925
Harold Booth <harold.booth@nist.gov>
2020-10-09 17:55:07 -0400

    added in CORs

--- server/api/src/config/index.js
+++ server/api/src/config/index.js
@@ -6,4 +6,5 @@ export default Object.freeze({
   , isDev
   , logLevel: isDev ? 'trace' : 'info'
   , maxQuerySize: 100 // 100 documents at a time
+  , CORSOriginsAccepted: process.env.CORS_ORIGINS_ACCEPTED ? process.env.CORS_ORIGINS_ACCEPTED.split(',') : []
 })
